3db7ab75650749ae8c880f02f6722262f325f244
Joseph Jimenez <blackbeltdiaries@gmail.com>
2018-05-14 18:09:26 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -23,7 +23,7 @@ class Scraper
   def self.scrape_profile_page(profile_url)
     doc = Nokogiri::HTML(open(profile_url))
 
-    profile = []
+    profile = {}
 
     social_sites = doc.css("a").collect {|v| v['ref']}
 
