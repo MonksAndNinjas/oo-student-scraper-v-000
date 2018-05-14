012f5d2a3a7cc0280c958a7830b3358e0856952b
Joseph Jimenez <blackbeltdiaries@gmail.com>
2018-05-14 17:51:24 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,12 +4,25 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+    doc = Nokogiri::HTML(open(index_url))
+
+    roster = []
+
+    names = doc.css(".student-name").collect {|v| name.text}
+    location = doc.css(".student-location").collect {|v| location.text}
+    profile_url = doc.css("div.student-card a").collect {|v| url['href']}
+
+      i = 0
+    while i < names.size
+      roster << {:name => names[i], :location => location[i], :profile_url => profile_url[i]}
+      i+=1
+    end
+    roster
   end
 
   def self.scrape_profile_page(profile_url)
-    
+    doc = Nokogiri::HTML(open(profile_url))
+    binding.pry
   end
 
 end
-
